c285624c66767a7cedc6da01c66e41e77761baac
Vamsi Krishna B <vamsi@fastmail.com>
2021-07-17 22:51:18 +0530

    Allow port to be configured via ENV

--- config/releases.exs
+++ config/releases.exs
@@ -40,7 +40,7 @@ explorer_hostname =
     """
 
 config :zcash_explorer, ZcashExplorerWeb.Endpoint,
-  url: [host: explorer_hostname, port: String.to_integer(System.get_env("PORT") || "4000")],
+  url: [host: explorer_hostname, port: String.to_integer(System.get_env("EXPLORER_PORT") || "443")],
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
