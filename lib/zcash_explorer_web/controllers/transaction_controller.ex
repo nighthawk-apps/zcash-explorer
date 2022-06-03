71a91900ad23c67a53e1307052ecf3fd9f5c8c97
Vamsi Krishna B <vamsi@fastmail.com>
2022-06-03 15:05:46 +0530

    Added View Raw TX JSON

--- lib/zcash_explorer_web/controllers/transaction_controller.ex
+++ lib/zcash_explorer_web/controllers/transaction_controller.ex
@@ -6,4 +6,13 @@ defmodule ZcashExplorerWeb.TransactionController do
     tx_data = Zcashex.Transaction.from_map(tx)
     render(conn, "tx.html", tx: tx_data, page_title: "Zcash Transaction #{txid}")
   end
+
+  def get_raw_transaction(conn, %{"txid" => txid}) do
+    {:ok, tx} = Zcashex.getrawtransaction(txid, 1)
+    data = Poison.encode!(tx, pretty: true)
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, data)
+  end
 end
