0b01b8fc0ea0ac68af214639dd5b62013ee34fd0
Vamsi <vamsi@fastmail.com>
2023-12-04 12:10:39 +0530

    Make it work on elixir 1.15

--- mix.exs
+++ mix.exs
@@ -34,10 +34,10 @@ defmodule ZcashExplorer.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.11"},
       {:postgrex, ">= 0.0.0"},
-      {:ecto_psql_extras, "~> 0.2"},
+      {:ecto_psql_extras, "~> 0.7.14"},
       {:phoenix_html, "~> 3.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.6.5"},
